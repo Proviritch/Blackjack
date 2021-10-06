b0e91ce67ebb1f871842a2a583c4d4c682efca5b
Adir Romo <adirelpro@gmail.com>
2021-10-06 09:27:02 -0500

    Logré crear al bot que juega contigo

--- blackjack.js
+++ blackjack.js
@@ -19,30 +19,22 @@ let BARAJA;
 const boton_comenzar_juego = document.getElementById("comenzar_juego");
 const boton_pedir_carta = document.getElementById("pedir_carta");
 const boton_parar = document.getElementById("parar");
-let valor_carta, cartas, elegida, suma, suma2;
+let valor_carta, cartas, elegida, suma, suma2, desicion;
 
 const pedir_carta = () => {
     valor_carta = Math.trunc(Math.random()*ARREGLO.length);
-    /* console.log(numero_carta); */
     cartas = Math.trunc(Math.random()*4);
     while (cartas > BARAJA[valor_carta].length-1) {
         --cartas;
     }
     sumar_cartas();
-/*     while (valor_carta > BARAJA.length-1) {
-        --valor_carta;
-    }  */
     elegida = BARAJA[valor_carta][cartas];
     BARAJA[valor_carta].splice(cartas,1);
-/*     console.log(BARAJA[valor_carta].length===0); */
     if (BARAJA[valor_carta].length===0) {
         BARAJA.splice(valor_carta,1);
         ARREGLO.splice(valor_carta,1);
     }
     console.log(elegida);
-/*     console.log(valor_carta,cartas,elegida);
-    console.log(BARAJA);
-    console.log(ARREGLO); */
 }
 
 const sumar_cartas = () => {
@@ -59,7 +51,27 @@ const sumar_cartas = () => {
         boton_pedir_carta.removeEventListener("click", pedir_carta);
     }
 
-}
+};
+
+const parar_pedir_cartas = () => {
+    console.log("PARAR");
+    boton_pedir_carta.removeEventListener("click", pedir_carta);
+    suma2 = suma;
+    suma = 0;
+    while (suma < suma2 || desicion === 0) {
+        pedir_carta();
+        if (suma === 21) {
+            desicion = 1;
+            boton_parar.removeEventListener("click", parar_pedir_cartas);
+        }
+        if (suma === suma2) {
+            desicion = Math.round(Math.random());
+            console.log("desicion: ",desicion)
+        } else if (suma > suma2) {
+            desicion = 1;
+        }
+    }
+};
 
 boton_comenzar_juego.addEventListener("click", () => {
     ARREGLO = [...ARREGLO_ORIGINAL];
@@ -69,15 +81,8 @@ boton_comenzar_juego.addEventListener("click", () => {
     }
     console.log(ARREGLO, BARAJA);
     suma = 0;
-    boton_parar.addEventListener("click", () => {
-        boton_pedir_carta.removeEventListener("click", pedir_carta);
-        suma2 = suma;
-        suma = 0;
-        while (suma <= 21) {
-            pedir_carta();
-        }
-        
-    });
+    desicion = 0;
+    boton_parar.addEventListener("click", parar_pedir_cartas);
     boton_pedir_carta.addEventListener("click", pedir_carta);
     console.log("YA SALÍ");
 });
