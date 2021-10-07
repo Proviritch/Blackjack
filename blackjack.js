9e3e1770f9c550ce5889683f499ab35eea4449ab
Adir Romo <adirelpro@gmail.com>
2021-10-07 11:23:56 -0500

    Ahora el código te dice exactamente quien ganó y si hubo empate

--- blackjack.js
+++ blackjack.js
@@ -19,7 +19,7 @@ let BARAJA;
 const boton_comenzar_juego = document.getElementById("comenzar_juego");
 const boton_pedir_carta = document.getElementById("pedir_carta");
 const boton_parar = document.getElementById("parar");
-let valor_carta, cartas, elegida, suma, suma2, desicion;
+let valor_carta, cartas, elegida, suma, suma2, desicion,turno;
 
 const pedir_carta = () => {
     valor_carta = Math.trunc(Math.random()*ARREGLO.length);
@@ -38,16 +38,25 @@ const pedir_carta = () => {
 }
 
 const sumar_cartas = () => {
+    console.log("turno: ", turno);
     if (suma < 21) {
         suma = suma + ARREGLO[valor_carta];
         console.log(suma);
     }
     if (suma === 21) {
-        console.log("GANASTE");
+        if (turno === 0) {
+            console.log("GANASTE JUGADOR");
+        } else if (turno === 1) {
+            console.log("GANASTE IA");
+        }
         /* suma2 = suma; */
         boton_pedir_carta.removeEventListener("click", pedir_carta);
     } else if (suma > 21) {
-        console.log("perdiste");
+        if (turno === 0) {
+            console.log("perdiste jugador :(");
+        } else if (turno === 1) {
+            console.log("perdiste IA :(");
+        }
         boton_pedir_carta.removeEventListener("click", pedir_carta);
     }
 
@@ -55,6 +64,7 @@ const sumar_cartas = () => {
 
 const parar_pedir_cartas = () => {
     console.log("PARAR");
+    turno = 1;
     boton_pedir_carta.removeEventListener("click", pedir_carta);
     suma2 = suma;
     suma = 0;
@@ -62,15 +72,24 @@ const parar_pedir_cartas = () => {
         pedir_carta();
         if (suma === 21) {
             desicion = 1;
+            /* console.log("GANASTE IA"); */
             boton_parar.removeEventListener("click", parar_pedir_cartas);
         }
         if (suma === suma2) {
             desicion = Math.round(Math.random());
-            console.log("desicion: ",desicion)
+            console.log("desicion: ",desicion);
+            if (desicion === 1) {
+                console.log("EMPATE");
+            }
         } else if (suma > suma2) {
             desicion = 1;
+            if (suma < 22) {
+                console.log("GANASTE IA");
+            }
+            boton_parar.removeEventListener("click", parar_pedir_cartas);
         }
     }
+    //pasa esta función afuera para que cuando se ejecute esta también se borre
 };
 
 boton_comenzar_juego.addEventListener("click", () => {
@@ -82,6 +101,7 @@ boton_comenzar_juego.addEventListener("click", () => {
     console.log(ARREGLO, BARAJA);
     suma = 0;
     desicion = 0;
+    turno = 0;
     boton_parar.addEventListener("click", parar_pedir_cartas);
     boton_pedir_carta.addEventListener("click", pedir_carta);
     console.log("YA SALÍ");
